f55097dbeac8ff10b31a96953295d29f9a3f6e87
aaka123 <aakash.phoughat12@gmail.com>
2018-11-24 11:29:14 +0530

    checking branch

--- C_lern.c
+++ C_lern.c
@@ -3,11 +3,11 @@
 int main()
 {
     int out = 0, tot = 0, cnt = 0;
-    int val[] = {5, 54, 76, 91, 35, 27, 45, 15, 99, 0};
+    int val1[] = {5, 54, 76, 91, 35, 27, 45, 15, 99, 0};
 
     while(cnt < 10)
     {
-        out = val[cnt];
+        out = val1[cnt];
         tot = tot + 0xffffffff/out;
         cnt++;
     }
